71e17992db8f5dd108822548c0501026c11f34a1
emresoysuren <emresoysuren03@gmail.com>
2023-06-18 03:34:34 +0300

    added app logo and created the fundamental files

---
+++ lib/config/color_palette.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/painting.dart';
+
+class ColorPalette {
+  ColorPalette._();
+
+  static const Color red = Color(0xffE84855);
+  static const Color yellow = Color(0xffffff00);
+  static const Color white = Color(0xffffffff);
+  static const Color blue = Color(0xff574AE2);
+  static const Color offBlack = Color(0xff101010);
+  static const Color offWhite = Color(0xffBBBBBB);
+  static const Color black = Color(0xff000000);
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,9 @@
+import 'package:chupp/app.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
-  runApp(const MainApp());
-}
-
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
-  }
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+  runApp(const App());
 }
