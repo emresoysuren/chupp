44788b18d1b899a79236c5be3ce024894b25efef
emresoysuren <emresoysuren03@gmail.com>
2023-06-22 01:58:15 +0300

    added animation to poll item

--- lib/widgets/posts/poll/poll_item.dart
+++ lib/widgets/posts/poll/poll_item.dart
@@ -2,7 +2,7 @@ import 'package:chupp/config/styles.dart';
 import 'package:chupp/config/utils.dart';
 import 'package:flutter/material.dart';
 
-class PollItem extends StatelessWidget {
+class PollItem extends StatefulWidget {
   final String tag;
   final int vote;
   final bool reversed;
@@ -14,24 +14,47 @@ class PollItem extends StatelessWidget {
     this.reversed = false,
   });
 
+  @override
+  State<PollItem> createState() => _PollItemState();
+}
+
+class _PollItemState extends State<PollItem> {
+  double scale = 1;
+
+  Future<void> _animate() async {
+    setState(() => scale = 0.96);
+    await Future.delayed(animationDuration);
+    setState(() => scale = 1);
+  }
+
+  get animationDuration => const Duration(milliseconds: 120);
+
   EdgeInsets get _padding => const EdgeInsets.all(8);
 
   @override
   Widget build(BuildContext context) {
-    return Material(
-      color: Utils.theme.current.primaryItem,
-      clipBehavior: Clip.hardEdge,
-      shape: const StadiumBorder(),
-      child: InkWell(
-        enableFeedback: false,
-        customBorder: const StadiumBorder(),
-        onTap: () {},
-        child: Ink(
-          child: IntrinsicHeight(
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: reversed ? _content.reversed.toList() : _content,
+    return AnimatedScale(
+      scale: scale,
+      duration: animationDuration,
+      child: Material(
+        color: Utils.theme.current.primaryItem,
+        clipBehavior: Clip.hardEdge,
+        shape: const StadiumBorder(),
+        child: InkWell(
+          enableFeedback: false,
+          customBorder: const StadiumBorder(),
+          onTap: () {
+            _animate();
+            // Keep the _animate method on the top
+          },
+          child: Ink(
+            child: IntrinsicHeight(
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children:
+                    widget.reversed ? _content.reversed.toList() : _content,
+              ),
             ),
           ),
         ),
@@ -43,7 +66,7 @@ class PollItem extends StatelessWidget {
         Padding(
           padding: _padding,
           child: Text(
-            "#$tag",
+            "#${widget.tag}",
             style: Styles.itemTextImp,
             maxLines: 1,
             softWrap: false,
@@ -56,7 +79,7 @@ class PollItem extends StatelessWidget {
             padding: _padding,
             child: Center(
               child: Text(
-                vote.toString(),
+                widget.vote.toString(),
                 maxLines: 1,
                 style: Styles.textImp,
               ),
