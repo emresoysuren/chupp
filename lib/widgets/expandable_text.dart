66d48821892409575b9b38d40988cb97729ba100
emresoysuren <emresoysuren03@gmail.com>
2023-06-22 03:12:55 +0300

    added suffix to the expandable text widget

--- lib/widgets/expandable_text.dart
+++ lib/widgets/expandable_text.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:chupp/config/styles.dart';
 import 'package:flutter/material.dart';
 
@@ -17,17 +19,28 @@ class ExpandableText extends StatefulWidget {
 }
 
 class _ExpandableTextState extends State<ExpandableText> {
-  late bool expand = widget.expanded;
+  late bool expanded = widget.expanded;
+
+  int get maxCharacters => 100;
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       behavior: HitTestBehavior.opaque,
-      onTap: () => setState(() => expand = !expand),
-      child: Text(
-        widget.text,
-        style: widget.style ?? Styles.text,
-        maxLines: expand ? null : 3,
+      onTap: () => setState(() => expanded = !expanded),
+      child: RichText(
+        text: TextSpan(
+          children: [
+            TextSpan(
+              text: expanded
+                  ? widget.text
+                  : widget.text
+                      .substring(0, min(widget.text.length, maxCharacters)),
+              style: widget.style ?? Styles.text,
+            ),
+            if (!expanded) TextSpan(text: " See More", style: Styles.subText),
+          ],
+        ),
       ),
     );
   }
