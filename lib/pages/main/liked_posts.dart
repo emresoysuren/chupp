515b09dadb83aec68e9fbeaf84c01fe8461699db
emresoysuren <emresoysuren03@gmail.com>
2023-06-28 01:40:46 +0300

    added active tap method to the liked posts page

--- lib/pages/main/liked_posts.dart
+++ lib/pages/main/liked_posts.dart
@@ -7,17 +7,32 @@ import 'package:chupp/widgets/posts/post/mini_post.dart';
 import 'package:flutter/material.dart';
 
 @RoutePage()
-class LikedPostsPage extends StatelessWidget {
+class LikedPostsPage extends StatefulWidget {
   const LikedPostsPage({super.key});
+
+  @override
+  State<LikedPostsPage> createState() => _LikedPostsPageState();
+}
+
+class _LikedPostsPageState extends State<LikedPostsPage> {
+  final ScrollController _controller = ScrollController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: const TitleBar(title: "Liked Posts"),
       backgroundColor: context.theme.current.primaryBg,
-      bottomNavigationBar: const AppNavBar(current: 2),
+      bottomNavigationBar: AppNavBar(
+        current: 2,
+        activeTap: () => _controller.animateTo(
+          0,
+          duration: const Duration(milliseconds: 320),
+          curve: Curves.ease,
+        ),
+      ),
       body: DisableScrollBehavior(
         child: ListView.builder(
+          controller: _controller,
           itemCount: 4,
           itemBuilder: (context, index) => const MiniPost(),
         ),
