47933090ec6588fe224432cdf1d4392046a746ba
emresoysuren <emresoysuren03@gmail.com>
2023-07-03 22:57:43 +0300

    made the auth pages resizeable

--- lib/widgets/auth/auth_base.dart
+++ lib/widgets/auth/auth_base.dart
@@ -2,7 +2,7 @@ import 'package:chupp/config/texts.dart';
 import 'package:chupp/utils/utils/context_extension.dart';
 import 'package:flutter/material.dart';
 
-class AuthBase extends StatelessWidget {
+class AuthBase extends StatefulWidget {
   final List<Widget> top;
   final List<Widget> bottom;
 
@@ -13,52 +13,68 @@ class AuthBase extends StatelessWidget {
   });
 
   @override
+  State<AuthBase> createState() => _AuthBaseState();
+}
+
+class _AuthBaseState extends State<AuthBase> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: context.theme.current.primaryBg,
-      resizeToAvoidBottomInset: false,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+      resizeToAvoidBottomInset: true,
+      body: SizedBox(
+        height: MediaQuery.of(context).size.height -
+            MediaQuery.of(context).viewInsets.bottom,
+        child: Center(
+          child: SingleChildScrollView(
+            physics: const NeverScrollableScrollPhysics(),
+            padding: const EdgeInsets.symmetric(vertical: 32),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: top,
-            ),
-          ),
-          if (top.isNotEmpty && bottom.isNotEmpty)
-            Stack(
-              clipBehavior: Clip.none,
-              alignment: Alignment.center,
               children: [
-                Container(
-                  color: context.theme.current.secondaryBg,
-                  height: 1,
-                  width: double.infinity,
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: widget.top,
+                  ),
                 ),
-                ColoredBox(
-                  color: context.theme.current.primaryBg,
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 16),
-                    child: Text(
-                      Texts.authOrDivider,
-                      style: context.styles.title3
-                          .copyWith(color: context.theme.current.subText),
-                    ),
+                if (widget.top.isNotEmpty && widget.bottom.isNotEmpty)
+                  Stack(
+                    clipBehavior: Clip.none,
+                    alignment: Alignment.center,
+                    children: [
+                      Container(
+                        color: context.theme.current.secondaryBg,
+                        height: 1,
+                        width: double.infinity,
+                      ),
+                      ColoredBox(
+                        color: context.theme.current.primaryBg,
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 16),
+                          child: Text(
+                            Texts.authOrDivider,
+                            style: context.styles.title3
+                                .copyWith(color: context.theme.current.subText),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 16, vertical: 32),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: widget.bottom,
                   ),
                 ),
               ],
             ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 32),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: bottom,
-            ),
-          )
-        ],
+          ),
+        ),
       ),
     );
   }
